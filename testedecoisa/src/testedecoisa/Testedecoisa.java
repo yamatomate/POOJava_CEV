783231307956e19bb13f579e1e8054fb099a7470
Gustavo Lisboa <130182272+yamatomate@users.noreply.github.com>
2024-01-05 14:24:58 -0300

    Add files via upload

---
+++ testedecoisa/src/testedecoisa/Testedecoisa.java
@@ -0,0 +1,12 @@
+
+package testedecoisa;
+
+public class Testedecoisa {
+
+    public static void main(String[] args) {
+    int idade = 30;
+    String valor = Integer.toString(idade);
+        System.out.println(valor);
+    }
+    
+}
---
+++ thayneretro/src/thayneretro/autista.java
@@ -0,0 +1,20 @@
+
+package thayneretro;
+
+import java.util.Scanner;
+
+public class autista {
+
+    public static void main(String[] args) {
+        /*Scanner teclado = new Scanner(System.in);
+        System.out.println("Qual seu nome?");
+        String nome = teclado.nextLine();
+        System.out.println("Qual sua nota?");
+        float nota = teclado.nextFloat();
+     System.out.format("A nota do %s nota eh %.1f \n",nome,nota);
+     */
+       String gg = "awawa";
+        System.out.println(gg.equals("CC"));
+    }
+    
+}
